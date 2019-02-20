ca131e17a309b9c0891bbfdc96383ccccaf88a28
Phil Dennis-Jordan <phil@philjordan.eu>
2019-02-20 21:37:13 +0100

    Fix for vnode function mocking.
     * Adds a macro KEXT_UNIT_TESTING_TESTABLE_KEXT_TARGET that's only #defined when building the kext itself for testing, not the target containing the tests. This keeps mock definitions out of the kext itself.
     * When building the tests, ensure we #include the kernel version of vnode.h as that defines some missing types. The #include guard matches that in the /usr/include version of the file, so we must make sure the #include directive for the kernel one is encountered before any user space one.
     * Some clean-up/comments to tidily separate mock declarations/definitions from the testable kext build itself.

--- ProjFS.Mac/PrjFSKext/KauthHandlerTestable.hpp
+++ ProjFS.Mac/PrjFSKext/KauthHandlerTestable.hpp
@@ -1,8 +1,7 @@
 #include "public/PrjFSCommon.h"
-#include <sys/kernel_types.h>
-#include <sys/vnode.h>
-#include "../PrjFSKextTests/KextLogMock.h"
 #include "../PrjFSKextTests/VnodeMock.h"
+#include <sys/kernel_types.h>
+#include "../PrjFSKextTests/KextLogMock.h"
 
 #ifndef __cplusplus
 #error None of the kext code is set up for being called from C or Objective-C; change the including file to C++ or Objective-C++
--- ProjFS.Mac/PrjFSKextTests/VnodeMock.h
+++ ProjFS.Mac/PrjFSKextTests/VnodeMock.h
@@ -1,15 +1,44 @@
 #ifndef VnodeMock_h
 #define VnodeMock_h
 
+
+#ifdef KEXT_UNIT_TESTING_TESTABLE_KEXT_TARGET
+// Building the kext code itself for testing
+#include <sys/vnode.h>
+
+#else
+// Building the test case target; pull in kernel vnode.h, not /usr/include version which is missing some bits
+
+// Dummy declarations to make the kernel vnode.h below compile in user space (Note: these are
+typedef struct ipc_port* ipc_port_t;
+enum uio_seg {};
+enum uio_rw {};
+
+#include <Kernel/sys/vnode.h>
+#endif
+
+
+// Mock declarations should be visible in both testable kext target and test case target
+extern "C"
+{
+    int vnode_getattr(vnode_t vp, struct vnode_attr *vap, vfs_context_t ctx);
+    int vnode_isrecycled(vnode_t vp);
+    enum vtype vnode_vtype(vnode_t vp);
+}
+
+
+// No mock definitions when building the kext itself for testing
+#ifndef KEXT_UNIT_TESTING_TESTABLE_KEXT_TARGET
+
+
 struct vnode {
     bool isrecycled;
     int attr;
     vtype vnodeType;
 };
 
-int vnode_getattr(vnode_t vp, struct vnode_attr *vap, vfs_context_t ctx);
-int vnode_isrecycled(vnode_t vp);
-enum vtype vnode_vtype(vnode_t vp);
+
+// TODO: put function bodies in a .cpp
 
 int vnode_getattr(vnode_t vp, struct vnode_attr *vap, vfs_context_t ctx)
 {
@@ -26,5 +55,7 @@ enum vtype vnode_vtype(vnode_t vp)
 {
     return vp->vnodeType;
 }
+
+#endif
 
 #endif
